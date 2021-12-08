37c8151923fd66d1b60433ef74b01efe913dcbc7
Samuel Serfaty <serfaty.samuel90@gmail.com>
2021-12-08 13:31:25 -0300

    Prueba form

--- php/send2.php
+++ php/send2.php
@@ -8,8 +8,8 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
