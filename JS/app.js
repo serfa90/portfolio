c87d5f240d7a0a9623bb43aecc3431bf33d75367
Samuel Serfaty <serfaty.samuel90@gmail.com>
2021-12-08 01:34:35 -0300

    Added flip card

--- JS/app.js
+++ JS/app.js
@@ -51,6 +51,62 @@ $("#iDo3").mouseleave(function(){
     $("#lineColor3").animate({width: '25px'})
 })
 
+
+/*||||||||||||||||||||| FLIP CARD EFFECT ||||||||||||||||||||||||*/
+
+const card1 = document.querySelector('#iDo1');
+
+card1.addEventListener('click', function(){
+    card1.classList.toggle('isFlipped');
+})
+
+
+const card2 = document.querySelector('#iDo2');
+
+card2.addEventListener('click', function(){
+    card2.classList.toggle('isFlipped');
+})
+
+
+const card3 = document.querySelector('#iDo3');
+
+card3.addEventListener('click', function(){
+    card3.classList.toggle('isFlipped');
+})
+
+/*||||||||||||||||| TAP HERE EFFECT ||||||||||||||||||||||||*/
+
+$("#iDo1").mouseenter(function(){
+    $("#tapHere1").show({display: 'block'})
+})
+$("#iDo1").click(function(){
+    $("#tapHere1").hide()
+})
+$("#iDo1").mouseleave(function(){
+    $("#tapHere1").hide()
+})
+
+$("#iDo2").mouseenter(function(){
+    $("#tapHere2").show({display: 'block'})
+})
+$("#iDo2").click(function(){
+    $("#tapHere2").hide()
+})
+$("#iDo2").mouseleave(function(){
+    $("#tapHere2").hide()
+})
+
+
+$("#iDo3").mouseenter(function(){
+    $("#tapHere3").show({display: 'block'})
+})
+$("#iDo3").click(function(){
+    $("#tapHere3").hide()
+})
+$("#iDo3").mouseleave(function(){
+    $("#tapHere3").hide()
+})
+
 /*||||||||||||||||||| TO TOP BUTTON |||||||||||||||||*/
 
 let topButton = document.getElementById("topBtn");
@@ -69,3 +125,13 @@ function topFunction(){
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+/*|||||||||||||| WHATSAPP BUTTON ||||||||||||*/
+
+$("#whatsappBtn").mouseenter(function(){
+    $("#wspImg").animate({width: '3.3em'})
+})
+
+$("#whatsappBtn").mouseleave(function(){
+    $("#wspImg").animate({width: '3em'})
+})
