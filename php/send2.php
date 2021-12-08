d361848327deb7e09a37af57b54ca2b87be7b8a3
Samuel Serfaty <serfaty.samuel90@gmail.com>
2021-12-08 13:48:52 -0300

    Prueba form

--- php/send2.php
+++ php/send2.php
@@ -1,7 +1,9 @@
 <?php
 $nombre = $_POST['name'];
+$apellido = $_POST['lastName']
 $mail = $_POST['email'];
-$empresa = $_POST['message'];
+$phone = $_POST['phone']
+$message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -10,6 +12,7 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $name . ",\r\n";
 $mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Su celular es: " . $phone . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['message'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
